b6872b8de2736e134687e619d3eb86a48a560120
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-08 15:14:41 +0200

    add new paper for JS

---
+++ main.js
@@ -0,0 +1,12 @@
+const name = prompt("Come ti hanno chiamato i tuoi genitori?")
+console.log(name)
+
+const surname = prompt("Come fa di cognome tuo Papà?")
+console.log(surname)
+
+const prefer_color = prompt("Qual'è il tuo colore preferito?")
+console.log(prefer_color)
+
+let number = 21
+
+document.getElementById('andrea').innerHTML = name + surname + prefer_color + number
